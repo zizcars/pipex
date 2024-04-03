c7840ddd6d60697d4885e40a8434bbb835794d8f
Abdelaziz Chakkaf <achakkaf@c2r3p8.1337.ma>
2024-04-03 21:16:45 +0000

    base idea of bonus

--- test.c
+++ test.c
@@ -6,86 +6,97 @@
 /*   By: Achakkaf <zizcarschak1@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 17:03:38 by Achakkaf          #+#    #+#             */
-/*   Updated: 2024/04/02 18:10:54 by Achakkaf         ###   ########.fr       */
+/*   Updated: 2024/04/03 21:15:47 by Achakkaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "mylib/get_next_line.h"
-#include <stdlib.h>
+#include "pipex_bonus.h"
 
-int main(int ac, char **av)
+int main(int ac, char **av, char **env)
 {
-	int pipefd[2][2];
-	pid_t pid[3];
-	char *cmd1[] = {"sort", NULL};
-	char *cmd2[] = {"uniq", "-u", NULL};
-	char *cmd3[] = {"grep", "-v", "error", NULL};
+	int pipfd[ac - 4][2];
+	int pip_i;
 	int i;
+	int pip;
+	int j;
 
 	i = 0;
-	while (i < 2)
+	while (i < ac - 4)
+		pipe(pipfd[i++]);
+	pip_i = 0;
+	pip = 0;
+	if (fork() == 0)
 	{
-		if(pipe(pipefd[i]) == -1)
+		int fd;
+
+		fd = open(av[1], O_RDONLY);
+		if (fd < 0)
 		{
-			perror("problem in pipe");
+			write(STDERR, "CAN'T OPEN FILE", 15);
 			exit(1);
 		}
+		redirection(fd, STDIN);
+		redirection(pipfd[0][1], STDOUT);
+		j = 0;
+		while (j < ac - 4)
+		{
+			close(pipfd[j][0]);
+			close(pipfd[j][1]);
+			j++;
+		}
+		exec_command(av[2], env);
+	}
+	i = 3;
+	while (i < ac - 2)
+	{
+		if (fork() == 0)
+		{
+			redirection(pipfd[pip_i][0], STDIN);
+			redirection(pipfd[pip_i + 1][1], STDOUT);
+			j = 0;
+			while (j < ac - 4)
+			{
+				close(pipfd[j][0]);
+				close(pipfd[j][1]);
+				j++;
+			}
+			exec_command(av[i], env);
+		}
 		i++;
+		pip_i++;
 	}
-	pid[0] = fork();
-	if (pid[0] == 0)
+	if (fork() == 0)
 	{
-		close(pipefd[0][0]);
-		close(pipefd[1][0]);
-		close(pipefd[1][1]);
-		dup2(pipefd[0][1], STDOUT_FILENO);
-		close(pipefd[0][1]);
-		execve("/usr/bin/sort", cmd1, NULL);
-		perror("execve");
-	}
-	pid[1] = fork();
-	if (pid[1] == 0)
-	{
-		close(pipefd[0][1]);
-		close(pipefd[1][0]);
-		dup2(pipefd[0][0], STDIN_FILENO);
-		close(pipefd[0][0]);
-		dup2(pipefd[1][1], STDOUT_FILENO);
-		close(pipefd[1][1]);
-		execve("/usr/bin/uniq", cmd2, NULL);
-		perror("execve");
-	}
-	pid[2] = fork();
-	if (pid[2] == 0)
-	{
-		close(pipefd[1][1]);
-		close(pipefd[0][0]);
-		close(pipefd[0][1]);
-		dup2(pipefd[1][0], STDIN_FILENO);
-		close(pipefd[1][0]);
-		execve("/usr/bin/grep", cmd3, NULL);
-		perror("execve");
-	}
-	else
-	{
+		int fd;
 
-		int status;
-		i = 0;
-		while(i < 2)
+		fd = open(av[ac - 1], O_CREAT | O_WRONLY, 0666);
+		if (fd < 0)
 		{
-			close(pipefd[i][0]);
-			close(pipefd[i][1]);
-			i++;
+			write(STDERR, "CAN'T OPEN FILE", 15);
+			exit(1);
 		}
-		i = 0;
-		while (i < 3)
+		redirection(pipfd[pip_i][0], STDIN);
+		redirection(fd, STDOUT);
+		j = 0;
+		while (j < ac - 4)
 		{
-			waitpid(pid[i], &status, 0);
-			i++;
+			close(pipfd[j][0]);
+			close(pipfd[j][1]);
+			j++;
 		}
-		exit(status);
+		exec_command(av[i], env);
+	}
+	j = 0;
+	while (j < ac - 4)
+	{
+		close(pipfd[j][0]);
+		close(pipfd[j][1]);
+		j++;
+	}
+	i = 2;
+	while (i < ac - 3)
+	{
+		wait(NULL);
+		i++;
 	}
 }
